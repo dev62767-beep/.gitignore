efee2ed8729e3155d5218f958860317d28aeada2
dev62767-beep <dev62767@gmail.com>
2025-10-26 20:18:07 +0530

    musiclibrary.py

--- musiclibrary.py
+++ musiclibrary.py
@@ -2,3 +2,4 @@ music = {
     "chikni chameli": "https://www.youtube.com/watch?v=QcQpqWhTBCE&list=RDQcQpqWhTBCE&start_radio=1&ab_channel=SonyMusicIndia",
     "jenny": "https://www.youtube.com/watch?v=hyj4JFSErrw&list=RDhyj4JFSErrw&start_radio=1&ab_channel=StudioKillers"
 }
+//add whatever amount of musics you like to add here
