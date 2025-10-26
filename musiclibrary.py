e28a4c0bfaf64b8789b8a6f6c9bfdbc0050332bb
dev62767-beep <dev62767@gmail.com>
2025-10-26 20:17:06 +0530

    musiclibrary.py

--- musiclibrary.py
+++ musiclibrary.py
@@ -1,4 +1,4 @@
 music = {
-    "chikni chameli chitra": "https://www.youtube.com/watch?v=QcQpqWhTBCE&list=RDQcQpqWhTBCE&start_radio=1&ab_channel=SonyMusicIndia",
+    "chikni chameli": "https://www.youtube.com/watch?v=QcQpqWhTBCE&list=RDQcQpqWhTBCE&start_radio=1&ab_channel=SonyMusicIndia",
     "jenny": "https://www.youtube.com/watch?v=hyj4JFSErrw&list=RDhyj4JFSErrw&start_radio=1&ab_channel=StudioKillers"
 }
